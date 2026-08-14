2bd33577a1942cc9a4bd18eb575cc9d5381d83dd
Vishal Chaudhary <kvcchaudhary007@gmail.com>
2026-08-14 22:00:40 +0530

    adding ui changes of Learning

---
+++ client/src/app/core/services/shop.service.ts
@@ -0,0 +1,47 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Pagination } from '../../shared/models/pagination';
+import { Product } from '../../shared/models/product';
+import { ShopParams } from '../../shared/models/shopparams';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ShopService {
+  baseUrl = 'https://localhost:5001/api/'
+  private http= inject(HttpClient);
+  types :string[]=[];
+  brands :string[]=[];
+
+  getProducts(shopParams: ShopParams) {
+    let params= new HttpParams();
+     if(shopParams.brands && shopParams.brands.length>0){
+      params =params.append('brands',shopParams.brands.join(','));
+     }
+     if(shopParams.types && shopParams.types.length>0){
+      params =params.append('types',shopParams.types.join(','));
+     }
+     if(shopParams.sort){
+      params =params.append('sort',shopParams.sort);
+     }
+     if(shopParams.search){
+      params =params.append('search',shopParams.search);
+     }
+     params= params.append('pageSize',shopParams.pageSize);
+     params= params.append('pageIndex',shopParams.pageNumber);
+
+    return this.http.get<Pagination<Product>>(this.baseUrl + 'product',{params});
+  }
+  getProduct(id: number) {
+    return this.http.get<Product>(this.baseUrl + 'product/' + id);
+  }
+
+  getBrands() {
+    if(this.brands.length > 0) return ;
+    return this.http.get<string[]>(this.baseUrl + 'product/brands').subscribe(response => this.brands = response);
+  }
+  getTypes() {
+    if(this.types.length > 0) return ;
+    return this.http.get<string[]>(this.baseUrl + 'product/types').subscribe(response => this.types = response);
+  }
+}
---
+++ client/src/app/features/shop/shop.component.ts
@@ -0,0 +1,93 @@
+import { Component, inject, OnInit } from '@angular/core';
+import { ShopService } from '../../core/services/shop.service';
+import { Product } from '../../shared/models/product';
+import { MatCard } from '@angular/material/card';
+import { ProductItemComponent } from "./product-item/product-item.component";
+import { MatDialog } from '@angular/material/dialog';
+import { FiltersDialogComponent } from './filters-dialog/filters-dialog.component';
+import { MatButton, MatIconButton } from '@angular/material/button';
+import { MatIcon } from '@angular/material/icon';
+import { MatMenu, MatMenuTrigger } from '@angular/material/menu';
+import { MatListOption, MatSelectionList, MatSelectionListChange } from '@angular/material/list';
+import { ShopParams } from '../../shared/models/shopparams';
+import { MatPaginator, PageEvent } from '@angular/material/paginator';
+import { Pagination } from '../../shared/models/pagination';
+import { FormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-shop',
+  imports: [MatCard, ProductItemComponent, MatButton, MatIcon,
+    MatMenu, MatSelectionList, MatListOption, MatMenuTrigger,
+    MatPaginator, FormsModule, MatIconButton],
+  templateUrl: './shop.component.html',
+  styleUrl: './shop.component.scss',
+})
+export class ShopComponent implements OnInit {
+private ShopService = inject(ShopService);
+private dialogService = inject(MatDialog);
+     products?: Pagination<Product>;
+
+    sortOptions=[
+      {name:'Alphabetical',value:'name'},
+      {name:'Price: Low to High',value:'priceAsc'},
+      {name:'Price: High to Low',value:'priceDesc'},  
+    ]
+    shopParams = new ShopParams();
+     pageSizeOptions = [5,10,20,50];
+   ngOnInit(): void {
+     this.initializeShop();
+   }
+   initializeShop() {
+     this.ShopService.getBrands();
+     this.ShopService.getTypes();
+     this.getProducts();
+   }
+
+   getProducts() {
+    this.ShopService.getProducts(this.shopParams).subscribe({
+       next: response => this.products = response,
+       error: error => console.error(error)
+     });
+   }
+   onSearchChange(){
+   this.shopParams.pageNumber = 1; // reset to first page when search changes
+   this.getProducts();
+   }
+   handlePageChange(event: PageEvent) {
+     this.shopParams.pageNumber = event.pageIndex + 1;  
+     this.shopParams.pageSize = event.pageSize;
+      this.getProducts();
+   }
+   onSortChange(event: MatSelectionListChange) {
+    const selectedOption = event.options[0];
+    if(selectedOption){
+      this.shopParams.sort=selectedOption.value;
+      this.shopParams.pageNumber = 1; // reset to first page when sorting changes
+      this.getProducts();
+    }
+   }
+    openFilterDialog() {
+     const dialogRef= this.dialogService.open(FiltersDialogComponent, {
+        minWidth: '500px',
+        data: {
+          selectedBrands: this.shopParams.brands,
+          selectedTypes: this.shopParams.types
+        }
+
+     });
+     dialogRef.afterClosed().subscribe({
+      next: result => {
+        if(result) {
+          this.shopParams.brands = result.selectedBrands;
+          this.shopParams.types = result.selectedTypes;
+          this.shopParams.pageNumber = 1; // reset to first page when filters change
+          // apply filter
+          this.getProducts();
+
+        }
+     }
+    });
+
+  }
+}
